e88d2fa90f8c80bf9aa90c6fb2996c3da33173d1
Molkobain <lajarige.guillaume@free.fr>
2019-07-22 22:49:56 +0200

    :package: Update module version to 1.4.0

--- src/molkobain-caselogs-toggler/module.molkobain-caselogs-toggler.php
+++ src/molkobain-caselogs-toggler/module.molkobain-caselogs-toggler.php
@@ -14,7 +14,7 @@
 /** @noinspection PhpUnhandledExceptionInspection */
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'molkobain-caselogs-toggler/1.3.2',
+	'molkobain-caselogs-toggler/1.4.0',
 	array(
 		// Identification
 		//
