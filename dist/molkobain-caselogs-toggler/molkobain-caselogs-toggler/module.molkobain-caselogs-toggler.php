544c127d77bb28e7c490e79568791280bd0b1e49
Molkobain <lajarige.guillaume@free.fr>
2019-07-22 22:52:40 +0200

    :package: Update redistribuables

--- dist/molkobain-caselogs-toggler/molkobain-caselogs-toggler/module.molkobain-caselogs-toggler.php
+++ dist/molkobain-caselogs-toggler/molkobain-caselogs-toggler/module.molkobain-caselogs-toggler.php
@@ -14,7 +14,7 @@
 /** @noinspection PhpUnhandledExceptionInspection */
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'molkobain-caselogs-toggler/1.3.2',
+	'molkobain-caselogs-toggler/1.4.0',
 	array(
 		// Identification
 		//
@@ -24,8 +24,9 @@ SetupWebPage::AddModule(
 		// Setup
 		//
 		'dependencies' => array(
-			'molkobain-handy-framework/1.1.0',
+			'molkobain-handy-framework/1.2.4',
 			'molkobain-fontawesome5-pack/1.1.1',
+			'molkobain-newsroom-provider/1.0.0',
 		),
 		'mandatory' => false,
 		'visible' => true,
